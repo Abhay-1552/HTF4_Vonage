f23af98417dca8d987dd828921ca63e51ef25b70
Abhay-1552 <gamerspropaganda143@gmail.com>
2024-02-10 19:33:45 +0530

    Working

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sms_api_voyage import API
 
 app = Flask(__name__, template_folder='template')
 
@@ -6,6 +7,21 @@ app = Flask(__name__, template_folder='template')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/submit_form', methods=['GET', 'POST'])
+def submit_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        data = API()
+        data.send_sms(name, email, message)
+        print(f"Name: {name}, Email: {email}, Message: {message}")
+
+        return render_template('index.html')
+    return '', 204
 
 
 if __name__ == "__main__":
--- sms_api_voyage.py
+++ sms_api_voyage.py
@@ -2,24 +2,28 @@ import os
 from dotenv import load_dotenv
 import vonage
 
-load_dotenv('.env')
 
-VONAGE_API_KEY = os.getenv("VONAGE_API_KEY")
-VONAGE_API_SECRET = os.getenv("VONAGE_API_SECRET")
-VONAGE_BRAND_NAME = os.getenv("VONAGE_BRAND_NAME")
-TO_NUMBER = int(os.getenv("TO_NUMBER"))
+class API:
+    def __init__(self):
+        load_dotenv('.env')
 
-client = vonage.Client(key=VONAGE_API_KEY, secret=VONAGE_API_SECRET)
+        self.VONAGE_API_KEY = os.getenv("VONAGE_API_KEY")
+        self.VONAGE_API_SECRET = os.getenv("VONAGE_API_SECRET")
+        self.VONAGE_BRAND_NAME = os.getenv("VONAGE_BRAND_NAME")
+        self.TO_NUMBER = int(os.getenv("TO_NUMBER"))
 
-responseData = client.sms.send_message(
-    {
-        "from": VONAGE_BRAND_NAME,
-        "to": TO_NUMBER,
-        "text": "A text message sent using the Vonage SMS API",
-    }
-)
+    def send_sms(self, name, mail, data):
+        client = vonage.Client(key=self.VONAGE_API_KEY, secret=self.VONAGE_API_SECRET)
 
-if responseData["messages"][0]["status"] == "0":
-    print("Message sent successfully.")
-else:
-    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+        responsedata = client.sms.send_message(
+            {
+                "from": self.VONAGE_BRAND_NAME,
+                "to": self.TO_NUMBER,
+                "text": f"{name} <{mail}>\n{data}",
+            }
+        )
+
+        if responsedata["messages"][0]["status"] == "0":
+            print("Message sent successfully.")
+        else:
+            print(f"Message failed with error: {responsedata['messages'][0]['error-text']}")
